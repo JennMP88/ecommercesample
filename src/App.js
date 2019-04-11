24fd786d40553d4db8b7f433c94e9de26a927557
Jennifer Perez <jenniferperez@pursuit.org>
2019-04-10 20:04:12 -0400

    made a map function to loop through my state

--- src/App.js
+++ src/App.js
@@ -6,11 +6,28 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-        products:[{
+        products:[
+        {
             name:'LV Belt',
             image:'https://us.louisvuitton.com/images/is/image/lv/1/PP_VP_AS/louis-vuitton--louis-vuitton--BG9U2PTX54_PM2_Front%20view.jpg?wid=305&hei=305',
             price:1000
-        }],
+        },
+        {
+            name:'LV Belt',
+            image:'https://us.louisvuitton.com/images/is/image/lv/1/PP_VP_AS/louis-vuitton--louis-vuitton--BG9U2PTX54_PM2_Front%20view.jpg?wid=305&hei=305',
+            price:1000
+        },
+        {
+            name:'LV Belt',
+            image:'https://us.louisvuitton.com/images/is/image/lv/1/PP_VP_AS/louis-vuitton--louis-vuitton--BG9U2PTX54_PM2_Front%20view.jpg?wid=305&hei=305',
+            price:1000
+        },
+        {
+            name:'LV Belt',
+            image:'https://us.louisvuitton.com/images/is/image/lv/1/PP_VP_AS/louis-vuitton--louis-vuitton--BG9U2PTX54_PM2_Front%20view.jpg?wid=305&hei=305',
+            price:1000
+        }
+    ],
         
     }
   }
@@ -18,15 +35,23 @@ class App extends React.Component {
  
   render() {
     // console.log(this.state);
+    // debugger
+    const {products}=this.state
     return (
       <>   
       <h1>ZAPATOS</h1>
         <div className='container row'> 
-            <div className='col-4 '>  {/* 1/3 of my screen */}
-                <img src={this.state.products[0].image} alt=''/>
-                <h4>{this.state.products[0].name}</h4>
-                <h5>{this.state.products[0].price}</h5>
-            </div>
+{  
+        products.map((e,i)=>{
+       return(     <div className='col-4 '>  {/* 1/3 of my screen */}
+            <img src={this.state.products[0].image} alt=''/>
+            <h4>{this.state.products[0].name}</h4>
+            <h5>{this.state.products[0].price}</h5>
+           </div>
+       )
+        })
+}
+            
         </div>
       </>
     )
